5fe90076dc0f97ce0254d336a61faf5cdedc440a
MSSohan <sohan.cu.eee.17@gmail.com>
2024-01-31 15:22:04 +0600

    up

--- Chapter 1/1.1.c
+++ Chapter 1/1.1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-main()
+
+void main()
 {
-printf(" MD Saifuzzaman Sohan \n Door no:2 \n Rahaman Nagar Residential Area \n 4 no. Road,B block \n Nasirabad,Chittagong\n\n\n");
-return 0;
+printf(" Md Saifuzzaman Sohan \n Door no:2 \n Rahaman Nagar Residential Area \n 4 no. Road,B block \n Nasirabad,Chittagong\n\n\n");
+// return 1;
 }
